870bed35a6eddcd4384b6120d7265dd274266725
Алёна Перова <perovaai31@yandex.ru>
2023-04-15 13:56:15 +0300

    Add new model and templates

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    # Запишите в переменную ice_cream_list новый QuerySet
+    ice_cream_list = IceCream.objects.values(
+                    'id', 'title', 'price', 'description'
+                    ).filter(
+                    is_published=True,
+                    is_on_main=True,
+                    category__is_published=True
+                    ).order_by(
+                    'output_order',
+                    'title')[:3]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
